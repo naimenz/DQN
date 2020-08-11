14f149462d5be9df7ea340ee2a8a566af22ed425
naimenz <ianmck98@gmail.com>
2020-08-11 16:00:07 +0100

    Random agent is working and state generation partially implemented

--- dqn.py
+++ dqn.py
@@ -14,13 +14,12 @@ all the important bits and share parameters and stuff.
 import numpy as np # using numpy as sparingly as possible, mainly for random numbers
 import torch
 import gym
+import matplotlib.pyplot as plt
 
 # set a seed for reproducibility during implementation
 torch.manual_seed(42)
 # using the new recommended numpy random number routines
 rng = np.random.default_rng(42)
-# make the environment
-env = gym.make('CartPole-v0')
 
 class DQN():
     """
@@ -29,9 +28,11 @@ class DQN():
     """
     # we initialise with an environment for now, might need to add architecture once
     # I get the NN working.
-    def __init__(self, env, eps):
-        self.eps = eps # initial epsilon for use in eps-greedy policy: random action is chosen eps of the time
+    def __init__(self, env, gamma, init_eps, eval_eps):
+        self.eps = init_eps # initial epsilon for use in eps-greedy policy: random action is chosen eps of the time
+        self.eval_eps = eval_eps # epsilon to be used at evaluation time (typically lower than training eps)
         self.env = env
+        self.gamma = gamma # discount rate (I think they used 0.99)
         self.n_acts = env.action_space.n
         # quickly get the dimensions of the images we will be using
         # unfortunately this will flicker the image on the screen at the moment
@@ -42,7 +43,7 @@ class DQN():
         self.state_dim = self.im_dim + (4,) # we will use 4 most recent frames as the states
 
         """
-         TO DO:
+         TODO:
          - Initialise Q network
          - Initialise optimiser for the Q network
         """
@@ -50,7 +51,7 @@ class DQN():
     # takes a batch of states of shape (batch, self.state_dim) as input and returns Q values as outputs
     def compute_Qs(self, s):
         """
-         TO DO:
+         TODO:
          - Compute Qs with neural network
         """
         # TODO For now, just return 0s across the board
@@ -69,19 +70,71 @@ class DQN():
             act = torch.randint(0, self.n_acts, (1,))
         return act
 
+    # preprocess a frame for use in constructing states
+    # NOTE TODO At the moment I am not actually using state information (random agent)
+    # so just return the frame unaltered
+    def preprocess_frame(self, frame):
+        """
+         TODO:
+         - Convert to greyscale
+         - Downsample/crop as needed
+        """
+        # TODO placeholder: just return frame
+        return frame
 
+    # encode the observation into a state based on the previous state and current obs
+    def get_state(self, s, obs):
+        processed_frame = self.preprocess_frame(obs)
+        # bump oldest frame and add new one
+        # we do this by dropping the first element of s and concatenating the new frame
+        # note that we have to unsqueeze the new frame so it has the same dimensions as s
+        sp = torch.cat((s[1:], processed_frame.unsqueeze(0)))
+        return sp
+    
+    # create an initial state by stacking four copies of the first frame
+    def initial_state(self, obs):
+        f = self.preprocess_frame(obs).unsqueeze(0)
+        s = torch.cat((f,f,f,f))
+        return s
 
-dqn = DQN(env)
-print(dqn.state_dim)
-# env.reset()
-# done = False
-# # while not done:
-# #     _, _, done, _ = env.step(0)
-# import time
-# tic = time.perf_counter()
-# x = env.render(mode='rgb_array')
-# toc = time.perf_counter()
-# print(f"render took {toc - tic:0.4f} seconds")
-# print(x)
-# env.close()
+    # run an episode in evaluation mode 
+    def evaluate(self):
+        # lists for logging
+        ep_states = []
+        ep_acts = []
+        ep_rews = []
 
+        # reset environment
+        done = False
+        _ = env.reset()
+        # get frame of the animation
+        obs = torch.from_numpy(env.render(mode='rgb_array').copy())
+        # because we only have one frame so far, just make the initial state 4 copies of it
+        s = self.initial_state(obs)
+
+        # loop over steps in the episode
+        while not done:
+            act = self.get_act(s, self.eval_eps) # returns a 1-element tensor
+            _, reward, done, info = env.step(act.item()) # throw away their obs
+
+            # log state, act, reward
+            ep_states.append(s)
+            ep_acts.append(act.item())
+            ep_rews.append(reward)
+
+            # get frame of the animation
+            obs = torch.from_numpy(env.render(mode='rgb_array').copy())
+            # construct new state from obsp
+            s = self.get_state(s, obs)
+
+        return ep_states, ep_acts, ep_rews
+
+# make the environment
+env = gym.make('CartPole-v0')
+# initialise agent
+dqn = DQN(env, gamma=0.99, init_eps=1., eval_eps=1.)
+ep_states, ep_acts, ep_rews = dqn.evaluate()
+for s in ep_states:
+    plt.imshow(s[-1])
+    plt.show()
+env.close()
